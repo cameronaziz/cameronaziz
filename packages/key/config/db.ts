e2ef3ebf8bb76a2d52b281c4b284a6d347732fb8
Cameron Aziz <cameronaziz@me.com>
2021-11-28 01:00:04 -0800

    work

--- packages/key/config/db.ts
+++ packages/key/config/db.ts
@@ -2,7 +2,7 @@ import type { ConfigOptions } from './types';
 
 const db: ConfigOptions['db'] = {
   provider: 'sqlite',
-  url: process.env.DATABASE_URL || 'file:./keystone-example.db',
+  url: process.env.DATABASE_URL || 'file:./keystone.db',
 };
 
 export default db;
